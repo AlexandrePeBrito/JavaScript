e290b5aa193401773beb91ee3f68c333db45de36
AlexandrePeBrito <88007398+AlexandrePeBrito@users.noreply.github.com>
2022-01-22 12:38:42 -0300

    Update comparacoes.js

--- estudo/fundamentos/comparacoes.js
+++ estudo/fundamentos/comparacoes.js
@@ -5,6 +5,8 @@ console.log(num==texto); //True pois so compara oq tem
 
 console.log(num===texto); //False pois compara o type tambem
 
+console.log(num+texto)          //Concatena
+console.log(num+Number(texto))
 
 //==========================Conversao
 
