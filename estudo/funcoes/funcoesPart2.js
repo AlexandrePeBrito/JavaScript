5dfe63b65f55659aa07dfbcb86a305f358c025d3
AlexandrePeBrito <88007398+AlexandrePeBrito@users.noreply.github.com>
2022-01-22 12:52:21 -0300

    Create funcoesPart2.js

---
+++ estudo/funcoes/funcoesPart2.js
@@ -0,0 +1,9 @@
+//1º - Metodo de escrever funçoes
+function    soma1(num1,num2){              
+    //codigo
+}
+
+//2º - Metodo de escrever funçoes
+const soma2 = function(num1,num2) {
+    return num1+num2
+}
