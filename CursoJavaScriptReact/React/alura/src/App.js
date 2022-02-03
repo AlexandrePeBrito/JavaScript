522735ec17257bd82bbebbf2bfff9637ce4d6eea
Alexandre Brito <alexandre_pereirabrito@yahoo.com.br>
2022-02-03 19:51:37 -0300

    estudo

--- CursoJavaScriptReact/React/alura/src/App.js
+++ CursoJavaScriptReact/React/alura/src/App.js
@@ -1,13 +1,17 @@
-import ListaDeNotas from './cp/listaNotas'
-import Cadastro from './cp/cadastro'
-
-function App() {
-  return (
-    <section>
-      <Cadastro/>
-      <ListaDeNotas/>
-    </section>
-  );
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro />
+        <ListaDeNotas />
+      </section>
+    );
+  }
 }
 
 export default App;
