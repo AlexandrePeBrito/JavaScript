f8e90eb6972591b223384366c0a1d94226a74ffe
Alexandre Brito <alexandre_pereirabrito@yahoo.com.br>
2022-02-03 21:11:39 -0300

    aprendendo componentes react

--- CursoJavaScriptReact/framework/alura/src/App.js
+++ CursoJavaScriptReact/framework/alura/src/App.js
@@ -4,11 +4,22 @@ import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
 import './assets/index.css';
 class App extends Component {
+
+  constructor() {
+    super();
+    this.notas=[]
+  }
+  criarNota(titulo,nota){
+    const notaAtt={titulo,nota}
+    this.notas.push(notaAtt)
+    console.log(this.notas.length)
+  }
+
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.notas}/>
       </section>
     );
   }
--- CursoJavaScriptReact/framework/alura/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ CursoJavaScriptReact/framework/alura/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,18 +1,40 @@
 import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
+  constructor(props) {
+    super(props)
+    this.titulo=''
+    this.nota=''
+  }
+
+  _mudancaTitulo(evento){
+    evento.stopPropagation()
+    this.titulo=evento.target.value
+  }
+  _mudancaNota(evento){
+    evento.stopPropagation()
+    this.nota=evento.target.value
+  }
+  _criarNota(evento){
+    evento.preventDefault()
+    evento.stopPropagation()
+    this.props.criarNota(this.titulo,this.nota)
+  }
   render() {
     return (
-      <form className="form-cadastro ">
+      <form className="form-cadastro "
+      onSubmit={this._criarNota.bind(this)}>
         <input
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
+          onChange={this._mudancaTitulo.bind(this)}
         />
         <textarea
           rows={15}
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
+          onChange={this._mudancaNota.bind(this)}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
